056a33bf086a6eb45eccd67b52479343289e18e1
Narayana Suri <narayanasuri08@gmail.com>
2018-02-12 08:39:32 -0800

    Added Particle js as background and integrated Google map API

--- js/main.js
+++ js/main.js
@@ -24,3 +24,15 @@ jQuery(document).ready(function ($) {
 		});
 	}
 });
+
+function initMap() {
+	var uluru = { lat: 13.021032, lng: 80.262312 };
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 16,
+		center: uluru
+	});
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
+}
